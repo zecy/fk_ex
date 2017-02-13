aae9a6d44125dc9f6b4a1a2948adc2a17d90b775
zecy <angelszc163@gmail.com>
2017-02-13 23:47:25 +0800

    供 tampermonkey 调用。

---
+++ fk_ex.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Fk Ex
+// @namespace    exh
+// @version      0.1
+// @description  get exhentai image url list
+// @author       zecy
+// @match        https://exhentai.org/g/*
+// @require      file:///Users/zecy/www/personal-scripts/chrom_user_script/assets/js/jquery-3.1.1.js
+// @require      file:///Users/zecy/www/personal-scripts/chrom_user_script/exhentai-image-list/main.js
+// @grant        none
+// ==/UserScript==
+
+main();
